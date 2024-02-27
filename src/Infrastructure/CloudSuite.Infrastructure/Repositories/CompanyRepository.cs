64ef544bc9114e8ceadd1251ef15360af4c3d3ed
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-02-27 13:45:03 -0300

    feat(#1337): building infrastucture repository from customers

---
+++ src/Infrastructure/CloudSuite.Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudSuite.Infrastructure.Context;
+using CloudSuite.Modules.Commons.ValueObjects;
+using CloudSuite.Modules.Domain.Contracts;
+using CloudSuite.Modules.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudSuite.Infrastructure.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+
+    {
+        protected readonly CustomerDbContext Db;
+        protected readonly DbSet<Company> DbSet;
+
+        public CompanyRepository(CustomerDbContext context)
+        {
+            Db = context;
+            DbSet = context.Company;
+        }
+        
+        public async Task<Company> GetByCnpj(Cnpj cnpj)
+        {
+            return await DbSet.FirstOrDefaultAsync(c => c.Cnpj == cnpj);
+        }
+
+        public async Task<IEnumerable<Company>> GetList()
+        {
+            return DbSet.ToList();
+        }
+
+        public void Remove(Company company)
+        {
+            DbSet.Remove(company);
+        }
+
+        public void Update(Company company)
+        {
+            DbSet.Update(company);
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+
+        public Task Add(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
