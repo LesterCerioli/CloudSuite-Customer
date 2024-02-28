37e42c4e584ebda9a53cba8f88c23ae4c53cc98c
Lucas Gabriel <lucas.barros@lucastechnologyservice.com>
2024-02-28 13:38:17 -0300

    feat(1339): building handlers from company

--- src/Modules/CloudSuite.Application/Handlers/Companies/CreateCompanyHandler.cs
+++ src/Modules/CloudSuite.Application/Handlers/Companies/CreateCompanyHandler.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CloudSuite.Application.Handlers.Companies.Responses;
+using CloudSuite.Modules.Commons.ValueObjects;
+using CloudSuite.Modules.Domain.Contracts;
+using MediatR;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
 
 namespace CloudSuite.Application.Handlers.Companies
 {
-    internal class CreateCompanyHandler
+    public class CreateCompanyHandler : IRequestHandler<CreateCompanyCommand, CreateCompanyResponse>
     {
+
+        private ICompanyRepository _repositorioCompany;
+        private readonly ILogger<CreateCompanyHandler> _logger;
+
+        public CreateCompanyHandler(ICompanyRepository repositorioCompany, ILogger<CreateCompanyHandler> logger)
+        {
+            _repositorioCompany = repositorioCompany;
+            _logger = logger;
+        }
+
+        public async Task<CreateCompanyResponse> Handle(CreateCompanyCommand command, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"CreateCompanyCommand: {JsonSerializer.Serialize(command)}");
+            var validationResult = new CreateCompanyCommandValidation().Validate(command);
+
+            if(validationResult.IsValid ) {
+                try
+                {
+                    var company = await _repositorioCompany.GetByCnpj(new Cnpj(command.Cnpj));
+                    if(company != null)
+                    {
+                        return await Task.FromResult(new CreateCompanyResponse(command.Id, "Company already registered."));
+                    }
+
+                    await _repositorioCompany.Add(command.GetEntity());
+                    return await Task.FromResult(new CreateCompanyResponse(command.Id, validationResult));
+                }catch(Exception ex)
+                {
+                    _logger.LogException(ex.Message);
+                    return await Task.FromResult(new CreateCompanyResponse(command.Id, "It wasn`t possible to process the solicitation."));
+                }
+            }
+
+            return await Task.FromResult(new CreateCompanyResponse(command.Id, validationResult));
+
+        }
     }
 }
--- src/Modules/CloudSuite.Application/Handlers/Companies/Responses/CheckCompanyExistsByCnpjAndFantasyNameAndSocialNameResponse.cs
+++ src/Modules/CloudSuite.Application/Handlers/Companies/Responses/CheckCompanyExistsByCnpjAndFantasyNameAndSocialNameResponse.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using CloudSuite.Application.Core;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CloudSuite.Application.Handlers.Companies.Responses
 {
-    internal class CheckCompanyByCnpjAndFantasyNameAndSocialNameResponse
+    public class CheckCompanyExistsByCnpjAndFantasyNameAndSocialNameResponse : Response
     {
+        public Guid RequestId {  get; private set; }
+        public bool Exists {  get; set; }
+
+        public CheckCompanyExistsByCnpjAndFantasyNameAndSocialNameResponse(Guid requestId, bool exists, ValidationResult result)
+        {
+            RequestId = requestId;
+            Exists = exists;
+            foreach(var item in result.Errors) {
+                this.AddError(item.ErrorMessage);
+            }
+        }
+
+        public CheckCompanyExistsByCnpjAndFantasyNameAndSocialNameResponse(Guid requestId, string validationFailure)
+        {
+            RequestId = requestId;
+            Exists = false;
+            this.AddError(validationFailure);
+        }
     }
 }
